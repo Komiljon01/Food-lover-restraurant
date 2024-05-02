87192100f9fcfbbeb1fa2537a260d127d23153fd
Komiljon <nortoshovk@gmail.com>
2024-05-02 12:30:03 +0500

    add classes to our special offers

--- js/script.js
+++ js/script.js
@@ -141,4 +141,88 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   const modalTimer = setTimeout(openModal, 6000);
+
+  // Classes
+  class OfferMenu {
+    constructor(src, alt, title, descr, discount, sale, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.discount = discount;
+      this.sale = sale;
+      this.parent = document.querySelector(parentSelector);
+      this.formatToUZS();
+    }
+
+    formatToUZS() {
+      this.discount = this.discount.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+      this.sale = this.sale.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    }
+
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `
+        <img src=${this.src} alt=${this.alt} />
+        <div>
+          <h3>${this.title}</h3>
+          <p>${this.descr}</p>
+          <p>
+            <del>${this.discount}</del> <span class="primary-text">${this.sale}</span>
+          </p>
+        </div>
+      `;
+
+      this.parent.append(element);
+    }
+  }
+
+  const offers = [
+    {
+      src: "./img/offer1.png",
+      alt: "Quattro Pasta",
+      title: "Quattro Pasta",
+      descr:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
+      discount: 55,
+      sale: 15,
+    },
+    {
+      src: "./img/offer2.png",
+      alt: "Vegertarian Pasta",
+      title: "Vegertarian Pasta",
+      descr:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
+      discount: 55,
+      sale: 22,
+    },
+    {
+      src: "./img/offer3.png",
+      alt: "Gluten-Free Pasta",
+      title: "Gluten-Free Pasta",
+      descr:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, quibusdam.",
+      discount: 55,
+      sale: 25,
+    },
+  ];
+
+  offers.forEach((offer) => {
+    const { src, alt, title, descr, discount, sale } = offer;
+    new OfferMenu(
+      src,
+      alt,
+      title,
+      descr,
+      discount,
+      sale,
+      ".offers-items"
+    ).render();
+  });
 });
